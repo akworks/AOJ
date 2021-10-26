f40f8afa90d77f18e8427541d61ca383aca25729
LION <LION@DESKTOP-8T0G604>
2021-10-26 14:18:40 +0900

    problem0000 add comment

--- src/volume0/problem0000/Main.java
+++ src/volume0/problem0000/Main.java
@@ -1,5 +1,9 @@
 package src.volume0.problem0000;
 
+/**
+ * Problem Set from Volume0
+ * Problem Title : 0000:   QQ
+ */
 public class Main {
 
     private static final int MAX = 9;
@@ -7,12 +11,16 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= MAX; i++) {
             for (int j = 1; j <= MAX; j++) {
+
+                // 九九の式を示す文字列の作成
                 StringBuilder sb = new StringBuilder();
                 sb.append(i);
                 sb.append("x");
                 sb.append(j);
                 sb.append("=");
                 sb.append(i * j);
+
+                // 1行出力
                 System.out.println(sb.toString());
             }
         }
